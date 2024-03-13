718f3ea38596a3033606acff4f74870ed03139d7
LoamySand <laneeboyd@gmail.com>
2024-03-13 15:47:31 -0500

    to section 6.70

---
+++ app/public/wp-content/themes/udemy/includes/front/head.php
@@ -0,0 +1,9 @@
+<?php
+
+function u_head(){
+    ?>
+    <!-- Improves performance -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <?php
+}
--- app/public/wp-content/themes/udemy/index.php
+++ app/public/wp-content/themes/udemy/index.php
@@ -9,7 +9,6 @@
 <body <?php body_class('example'); ?>>
     <?php wp_body_open(); ?>
     
-    <h1>Hello</h1>
 
     <?php wp_footer(); ?>
 </body>
